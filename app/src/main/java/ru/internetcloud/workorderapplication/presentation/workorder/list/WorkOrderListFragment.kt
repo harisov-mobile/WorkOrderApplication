1373b98e5ea7b6ce962d934ab90ef6d1e7879cd9
harisov-mobile <v8spec@mail.ru>
2023-04-07 08:04:30 +0200

    Fix leaking the workOrderRecyclerView

--- app/src/main/java/ru/internetcloud/workorderapplication/presentation/workorder/list/WorkOrderListFragment.kt
+++ app/src/main/java/ru/internetcloud/workorderapplication/presentation/workorder/list/WorkOrderListFragment.kt
@@ -55,7 +55,6 @@ class WorkOrderListFragment : Fragment(), FragmentResultListener {
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-
         // даггер:
         component.inject(this)
     }
@@ -63,6 +62,11 @@ class WorkOrderListFragment : Fragment(), FragmentResultListener {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_work_order_list, container, false)
         return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        workOrderRecyclerView.adapter = null
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
