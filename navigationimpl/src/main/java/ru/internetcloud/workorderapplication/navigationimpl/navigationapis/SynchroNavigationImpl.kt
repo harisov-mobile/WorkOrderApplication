aaab53f2cff942fa5737bd682a89a090e025e757
harisov-mobile <v8spec@mail.ru>
2024-01-06 14:50:15 +0300

    Refactor Provider<NavController> to NavController

--- navigationimpl/src/main/java/ru/internetcloud/workorderapplication/navigationimpl/navigationapis/SynchroNavigationImpl.kt
+++ navigationimpl/src/main/java/ru/internetcloud/workorderapplication/navigationimpl/navigationapis/SynchroNavigationImpl.kt
@@ -2,20 +2,19 @@ package ru.internetcloud.workorderapplication.navigationimpl.navigationapis
 
 import androidx.navigation.NavController
 import javax.inject.Inject
-import javax.inject.Provider
 import ru.internetcloud.workorderapplication.navigationapi.NavigationApi
 import ru.internetcloud.workorderapplication.synchro.presentation.DataSynchronizationFragmentDirections
 import ru.internetcloud.workorderapplication.synchro.presentation.navigation.SynchroDirections
 
 class SynchroNavigationImpl @Inject constructor(
-    private val navController: Provider<NavController>,
+    private val navController: NavController
 ): NavigationApi<SynchroDirections> {
 
     override fun navigate(direction: SynchroDirections) {
         when (direction) {
             SynchroDirections.ToWorkOrders -> {
                 // запустить фрагмент, где будет список заказ-нарядов
-                navController.get().navigate(DataSynchronizationFragmentDirections.actionDataSynchronizationFragmentToWorkOrderListFragment())
+                navController.navigate(DataSynchronizationFragmentDirections.actionDataSynchronizationFragmentToWorkOrderListFragment())
             }
         }
     }
--- navigationimpl/src/main/java/ru/internetcloud/workorderapplication/navigationimpl/navigationapis/WorkOrderDetailNavigationImpl.kt
+++ navigationimpl/src/main/java/ru/internetcloud/workorderapplication/navigationimpl/navigationapis/WorkOrderDetailNavigationImpl.kt
@@ -2,18 +2,17 @@ package ru.internetcloud.workorderapplication.navigationimpl.navigationapis
 
 import androidx.navigation.NavController
 import javax.inject.Inject
-import javax.inject.Provider
 import ru.internetcloud.workorderapplication.navigationapi.NavigationApi
 import ru.internetcloud.workorderapplication.workorderdetail.presentation.navigation.WorkOrderDetailDirections
 
 class WorkOrderDetailNavigationImpl @Inject constructor(
-    private val navController: Provider<NavController>,
+    private val navController: NavController
 ): NavigationApi<WorkOrderDetailDirections> {
 
     override fun navigate(direction: WorkOrderDetailDirections) {
         when (direction) {
             WorkOrderDetailDirections.Up -> {
-                navController.get().navigateUp()
+                navController.navigateUp()
             }
         }
     }
